0732288f881f4a5f95af61022607cf9a844882d3
Apoorv Nag <apoorv@tripoto.com>
2018-08-06 11:20:02 +0530

    PHP56 and Nginx

---
+++ setup.sh
@@ -0,0 +1,8 @@
+# Installing PHP5.6
+sudo add-apt-repository ppa:ondrej/php
+sudo apt-get update
+sudo apt-get install php5.6-fpm
+
+#Installing Nginx
+sudo apt-get update
+sudo apt-get install nginx
