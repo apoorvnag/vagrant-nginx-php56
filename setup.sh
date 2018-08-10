222ed650f338ea3d7af826a5694f13196e49480d
Apoorv <apoorv.nag@gmail.com>
2018-08-10 13:08:21 +0000

    Install Git Composer

--- setup.sh
+++ setup.sh
@@ -1,8 +1,25 @@
-# Installing PHP5.6
+# Install PHP5.6
 sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update
 sudo apt-get install php5.6-fpm
 
-#Installing Nginx
+# Install Nginx
 sudo apt-get update
 sudo apt-get install nginx
+
+# Install Git
+sudo add-apt-repository ppa:git-core/ppa
+sudo apt-get update
+sudo apt install git
+
+# Install Composer
+# curl in order to download Composer
+# php-cli for installing
+# The php-mbstring package is necessary to provide functions for a library we'll be using
+# git is used by Composer for downloading project dependencies
+# unzip for extracting zipped packages
+sudo apt-get update
+sudo apt-get install curl php-cli php-mbstring git unzip
+cd ~
+curl -sS https://getcomposer.org/installer -o composer-setup.php
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
