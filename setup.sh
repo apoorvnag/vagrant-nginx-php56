c5ec7e7e8432695976383ffb794ee2ce196ad9b2
Apoorv Nag <apoorv@tripoto.com>
2018-08-11 00:55:11 +0530

    PHP locale Issue NodeJS

--- setup.sh
+++ setup.sh
@@ -1,11 +1,29 @@
-# Install PHP5.6
-sudo add-apt-repository ppa:ondrej/php
+###########################################
+#
+# Ubuntu 14.04.5 LTS (GNU/Linux 3.13.0-153-generic x86_64)
+#
+###########################################
+
+
+# To fix no locale in Ubuntu
+sudo apt-get install -y language-pack-en-base
+sudo export LC_ALL=en_US.UTF-8
+sudo export LANG=en_US.UTF-8
+sudo apt-get install -y software-properties-common
 sudo apt-get update
-sudo apt-get install php5.6-fpm
+
 
 # Install Nginx
 sudo apt-get update
 sudo apt-get install nginx
+
+# Install PHP5.6
+sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php
+sudo apt-get update
+sudo add-apt-repository ppa:ondrej/php
+# sudo add-apt-repository ppa:ondrej/nginx-mainline
+sudo apt-get update
+sudo apt-get install php5.6-fpm
 
 # Install Git
 sudo add-apt-repository ppa:git-core/ppa
@@ -23,3 +41,20 @@ sudo apt-get install curl php-cli php-mbstring git unzip
 cd ~
 curl -sS https://getcomposer.org/installer -o composer-setup.php
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+rm composer-setup.php
+
+
+# Install Nodejs
+sudo apt-get update
+cd ~
+curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+rm nodesource_setup.sh
+sudo apt-get install nodejs
+nodejs -v
+npm -v
+sudo apt-get install build-essential
+
+
+
+
