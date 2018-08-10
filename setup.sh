ba29dffdb4c9ff665f1ba84589f189fd9449ab6a
Apoorv Nag <apoorv@tripoto.com>
2018-08-11 01:23:00 +0530

    php-mbstring version fix

--- setup.sh
+++ setup.sh
@@ -7,23 +7,23 @@
 
 # To fix no locale in Ubuntu
 sudo apt-get install -y language-pack-en-base
-sudo export LC_ALL=en_US.UTF-8
-sudo export LANG=en_US.UTF-8
+sudo apt-get update
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
 sudo apt-get install -y software-properties-common
 sudo apt-get update
-
-
-# Install Nginx
-sudo apt-get update
-sudo apt-get install nginx
 
 # Install PHP5.6
 sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php
 sudo apt-get update
-sudo add-apt-repository ppa:ondrej/php
+# sudo add-apt-repository ppa:ondrej/php
 # sudo add-apt-repository ppa:ondrej/nginx-mainline
-sudo apt-get update
+# sudo apt-get update
 sudo apt-get install php5.6-fpm
+
+# Install Nginx
+sudo apt-get update
+sudo apt-get install nginx
 
 # Install Git
 sudo add-apt-repository ppa:git-core/ppa
@@ -31,13 +31,13 @@ sudo apt-get update
 sudo apt install git
 
 # Install Composer
+# git installed already
+# php installed already
 # curl in order to download Composer
-# php-cli for installing
 # The php-mbstring package is necessary to provide functions for a library we'll be using
-# git is used by Composer for downloading project dependencies
 # unzip for extracting zipped packages
 sudo apt-get update
-sudo apt-get install curl php-cli php-mbstring git unzip
+sudo apt-get install curl php5.6-mbstring unzip
 cd ~
 curl -sS https://getcomposer.org/installer -o composer-setup.php
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
